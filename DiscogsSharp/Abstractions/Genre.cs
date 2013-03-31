942084251aea7026831db9c921116842960f119c
dmitry sergienko <dimansergiyenko@gmail.com>
2013-03-31 15:11:03 +0400

    Added genre class. Moved enums to separate folder.

---
+++ DiscogsSharp/Abstractions/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DiscogsSharp.Enums;
+
+namespace DiscogsSharp.Abstractions
+{
+    public class Genre
+    {
+        public Guid Id { get; private set; }
+        public Genres GenreName { get; private set; }
+
+        public Genre(Genres genreName)
+        {
+            Id = Guid.NewGuid();
+            GenreName = genreName;
+        }
+    }
+}
